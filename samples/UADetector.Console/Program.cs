853f90b1b2a53bb688c87c12a482f33c2004d934
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-02-28 23:23:33 +0100

    Remove custom dictionary

--- samples/UADetector.Console/Program.cs
+++ samples/UADetector.Console/Program.cs
@@ -1,16 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Collections.Frozen;
-
 using UADetector.Parsers;
 using UADetector.Regexes.Models;
-using UADetector.Utils;
 
 Console.WriteLine("Hello, World!");
 
-var result = ParserExtensions.LoadRegexes<OperatingSystemRegex>("Regexes.operating_systems.yml");
-
-var a = new FrozenBiDictionary<string, string>(new Dictionary<string, string> { });
+var result = ParserExtensions.LoadRegexes<OperatingSystemRegex>("Regexes.Resources.operating_systems.yml");
 
 
 Console.WriteLine(result);
