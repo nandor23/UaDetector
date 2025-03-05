32efbce28aace22a8d300a555924cbdeadee7e39
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-03-05 21:06:06 +0100

    Add Api sample project

---
+++ samples/UADetector.Api/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+using UADetector.Parsers;
+
+namespace UADetector.Api.Controllers;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly IOsParser _osParser;
+
+    
+    public TestController(IOsParser osParser)
+    {
+        _osParser = osParser;
+    }
+    
+    [Route("")]
+    public IActionResult Get()
+    {
+        var userAgent = HttpContext.Request.Headers.UserAgent.ToString();
+        _osParser.TryParse(userAgent, out var result);
+        
+        return Ok(result);
+    }
+}
