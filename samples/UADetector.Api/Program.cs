0ec9e951f4ef53d3a493797ab07ce075045e112b
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-03-06 06:17:57 +0100

    Remvoe comments from samples

--- samples/UADetector.Api/Program.cs
+++ samples/UADetector.Api/Program.cs
@@ -4,14 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IOsParser, OsParser>();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
--- samples/UADetector.Console/Program.cs
+++ samples/UADetector.Console/Program.cs
@@ -1,11 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using UADetector.Models.Enums;
-using UADetector.Parsers;
-using UADetector.Regexes.Models;
-
-Console.WriteLine("Hello, World!");
-
-//var result = ParserExtensions.LoadRegexes<Os>("Regexes.Resources.oss.yml", RegexPatternType.UserAgent);
-
-//Console.WriteLine(result.First());
+﻿Console.WriteLine("Hello, World!");
