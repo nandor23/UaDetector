66c11a888e1548a0543a93c9329985678e13cbb1
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-04-12 13:33:44 +0200

    Update feed reader fixtures

--- tests/UADetector.Tests/Fixtures/Models/ClientFixture.cs
+++ tests/UADetector.Tests/Fixtures/Models/ClientFixture.cs
@@ -1,13 +1,9 @@
+using UADetector.Results;
+
 namespace UADetector.Tests.Fixtures.Models;
 
 public class ClientFixture
 {
     public required string UserAgent { get; init; }
     public required ClientInfo Client { get; init; }
-
-    public class ClientInfo
-    {
-        public required string Name { get; init; }
-        public required string? Version { get; init; }
-    }
 }
