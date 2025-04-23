da1235db82c1a26889519dfcbfeb198812f14f40
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-04-23 12:33:56 +0200

    Test string extensions

---
+++ tests/UaDetector.Tests/Utils/StringExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+
+using UaDetector.Utils;
+
+namespace UaDetector.Tests.Utils;
+
+public class StringExtensionsTests
+{
+    [Test]
+    [MethodDataSource(nameof(CollapseSpacesTestData))]
+    public void CollapseSpaces_WithExtraSpaces_ReturnsCollapsedString(string input, string output)
+    {
+        input.CollapseSpaces().ShouldBe(output);
+    }
+
+    public static IEnumerable<Func<(string, string)>> CollapseSpacesTestData()
+    {
+        yield return () => ("Hello World", "Hello World");
+        yield return () => ("Hello World ", "Hello World");
+        yield return () => ("Hello World  ", "Hello World");
+        yield return () => (" Hello World", "Hello World");
+        yield return () => ("  Hello World", "Hello World");
+        yield return () => (" Hello   World   ", "Hello World");
+    }
+
+    [Test]
+    [MethodDataSource(nameof(RemoveSpacesTestData))]
+    public void RemoveSpaces_WithSpaces_ReturnsStringWithoutSpaces(string input, string output)
+    {
+        input.RemoveSpaces().ShouldBe(output);
+    }
+
+    public static IEnumerable<Func<(string, string)>> RemoveSpacesTestData()
+    {
+        yield return () => ("Hello World", "HelloWorld");
+        yield return () => ("Hello World ", "HelloWorld");
+        yield return () => ("Hello World  ", "HelloWorld");
+        yield return () => (" Hello World", "HelloWorld");
+        yield return () => ("  Hello World", "HelloWorld");
+        yield return () => (" Hello   World   ", "HelloWorld");
+    }
+}
