26183efac1a6a3a4f175fa7065a35dd33bbc22cd
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-04-25 04:05:01 +0200

    Update dependencies

--- samples/UaDetector.Api/Program.cs
+++ samples/UaDetector.Api/Program.cs
@@ -1,9 +1,13 @@
 using Scalar.AspNetCore;
 
+using UaDetector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IUaDetector>(new UaDetector.UaDetector());
 
 var app = builder.Build();
 
