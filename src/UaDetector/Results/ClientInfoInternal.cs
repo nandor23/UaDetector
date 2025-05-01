4f66a595a205a4186719e098c0739fe67eb0e53d
Nandor Krizbai <krizbai.nandor@gmail.com>
2025-05-01 13:32:49 +0200

    Update README

--- src/UaDetector/Results/ClientInfoInternal.cs
+++ src/UaDetector/Results/ClientInfoInternal.cs
@@ -1,6 +1,6 @@
 namespace UaDetector.Results;
 
-public class ClientInfoInternal
+public sealed class ClientInfoInternal
 {
     public required string Name { get; init; }
     public required string? Version { get; init; }
