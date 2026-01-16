baf1343995383535fca987b0d64c93fc53fb37bb
Nandor Krizbai <krizbai.nandor@gmail.com>
2026-01-16 10:14:32 +0100

    Fix version extraction from client hints

--- tests/UaDetector.Tests/Tests/UaDetectorTests.cs
+++ tests/UaDetector.Tests/Tests/UaDetectorTests.cs
@@ -42,9 +42,8 @@ public class UaDetectorTests
 
     [Test]
     [MethodDataSource(nameof(FixtureFileNames))]
-    public async Task TryParse_ShouldReturnExpectedUserAgentInfo(string fileName)
+    public async Task TryParse_ShouldReturnExpectedUserAgentInfo(string fixturePath)
     {
-        var fixturePath = Path.Combine("Fixtures", "Resources", "Collections", $"{fileName}.json");
         var fixtures = await FixtureLoader.LoadAsync<UserAgentFixture>(fixturePath);
         var uaDetector = new UaDetector(
             new UaDetectorOptions { VersionTruncation = VersionTruncation.None }
@@ -82,9 +81,8 @@ public class UaDetectorTests
 
     [Test]
     [MethodDataSource(nameof(FixtureFileNames))]
-    public async Task TryParse_ShouldProduceConsistentResultsAcrossParsers(string fileName)
+    public async Task TryParse_ShouldProduceConsistentResultsAcrossParsers(string fixturePath)
     {
-        var fixturePath = Path.Combine("Fixtures", "Resources", "Collections", $"{fileName}.json");
         var fixtures = await FixtureLoader.LoadAsync<UserAgentFixture>(fixturePath);
         var uaDetectorOptions = new UaDetectorOptions
         {
@@ -153,86 +151,11 @@ public class UaDetectorTests
 
     public static IEnumerable<Func<string>> FixtureFileNames()
     {
-        yield return () => "cameras";
-        yield return () => "car_browsers";
-        yield return () => "client_hints";
-        yield return () => "client_hints_apps";
-        yield return () => "consoles";
-        yield return () => "desktops";
-        yield return () => "desktops_1";
-        yield return () => "feature_phones";
-        yield return () => "feed_readers";
-        yield return () => "media_players";
-        yield return () => "mobile_apps";
-        yield return () => "peripherals";
-        yield return () => "phablets";
-        yield return () => "phablets_1";
-        yield return () => "podcasting_apps";
-        yield return () => "portable_media_players";
-        yield return () => "smart_displays";
-        yield return () => "smart_speakers";
-        yield return () => "smartphones";
-        yield return () => "smartphones_1";
-        yield return () => "smartphones_2";
-        yield return () => "smartphones_3";
-        yield return () => "smartphones_4";
-        yield return () => "smartphones_5";
-        yield return () => "smartphones_6";
-        yield return () => "smartphones_7";
-        yield return () => "smartphones_8";
-        yield return () => "smartphones_9";
-        yield return () => "smartphones_10";
-        yield return () => "smartphones_11";
-        yield return () => "smartphones_12";
-        yield return () => "smartphones_13";
-        yield return () => "smartphones_14";
-        yield return () => "smartphones_15";
-        yield return () => "smartphones_16";
-        yield return () => "smartphones_17";
-        yield return () => "smartphones_18";
-        yield return () => "smartphones_19";
-        yield return () => "smartphones_20";
-        yield return () => "smartphones_21";
-        yield return () => "smartphones_22";
-        yield return () => "smartphones_23";
-        yield return () => "smartphones_24";
-        yield return () => "smartphones_25";
-        yield return () => "smartphones_26";
-        yield return () => "smartphones_27";
-        yield return () => "smartphones_28";
-        yield return () => "smartphones_29";
-        yield return () => "smartphones_30";
-        yield return () => "smartphones_31";
-        yield return () => "smartphones_32";
-        yield return () => "smartphones_33";
-        yield return () => "smartphones_34";
-        yield return () => "smartphones_35";
-        yield return () => "smartphones_36";
-        yield return () => "smartphones_37";
-        yield return () => "smartphones_38";
-        yield return () => "smartphones_39";
-        yield return () => "smartphones_40";
-        yield return () => "smartphones_41";
-        yield return () => "tablets";
-        yield return () => "tablets_1";
-        yield return () => "tablets_2";
-        yield return () => "tablets_3";
-        yield return () => "tablets_4";
-        yield return () => "tablets_5";
-        yield return () => "tablets_6";
-        yield return () => "tablets_7";
-        yield return () => "tablets_8";
-        yield return () => "tablets_9";
-        yield return () => "tablets_10";
-        yield return () => "tablets_11";
-        yield return () => "tablets_12";
-        yield return () => "televisions";
-        yield return () => "televisions_1";
-        yield return () => "televisions_2";
-        yield return () => "televisions_3";
-        yield return () => "televisions_4";
-        yield return () => "televisions_5";
-        yield return () => "wearables";
-        yield return () => "unknown";
+        var fixturesPath = Path.Combine("Fixtures", "Resources", "Collections");
+    
+        foreach (var file in Directory.GetFiles(fixturesPath, "*.json"))
+        {
+            yield return () => file;
+        }
     }
 }
