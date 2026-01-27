a091a204dc524f23ab42e8c66401e1d878012f3a
Nandor Krizbai <krizbai.nandor@gmail.com>
2026-01-27 19:51:43 +0100

    Improve bot detection

--- src/UaDetector/Parsers/OsParser.cs
+++ src/UaDetector/Parsers/OsParser.cs
@@ -221,7 +221,7 @@ public sealed partial class OsParser : IOsParser
                     OsCode.RtosAndNext,
                     OsCode.KolibriOs,
                     OsCode.MocorOs,
-                    OsCode.NuttX
+                    OsCode.NuttX,
                 }.ToFrozenSet()
             },
             {
