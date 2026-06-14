b291701a1c2444143b74ee7cbbeae34ce748b2d6
Nandor Krizbai <63061477+nandor23@users.noreply.github.com>
2026-06-14 18:25:38 +0200

    refactor: convert models to records (#231)

--- src/UaDetector.Abstractions/Models/EngineInfo.cs
+++ src/UaDetector.Abstractions/Models/EngineInfo.cs
@@ -1,19 +1,7 @@
 namespace UaDetector.Abstractions.Models;
 
-public sealed class EngineInfo
+public sealed record EngineInfo
 {
     public string? Name { get; init; }
     public string? Version { get; init; }
-
-    public override string ToString()
-    {
-        return string.Join(
-            ", ",
-            new[]
-            {
-                string.IsNullOrEmpty(Name) ? null : $"{nameof(Name)}: {Name}",
-                string.IsNullOrEmpty(Version) ? null : $"{nameof(Version)}: {Version}",
-            }.Where(x => !string.IsNullOrEmpty(x))
-        );
-    }
 }
