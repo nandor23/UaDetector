ff037fa82a2a1535eb1dc8c80d10210525aed8dd
Nandor Krizbai <krizbai.nandor@gmail.com>
2026-07-03 12:41:26 +0200

    Fix test

--- tests/UaDetector.Abstractions.Tests/Enums/OsCodeTests.cs
+++ tests/UaDetector.Abstractions.Tests/Enums/OsCodeTests.cs
@@ -226,7 +226,7 @@ public class OsCodeTests
             { OsCode.HarmonyOsNext, 203 },
             { OsCode.AndroidGo, 204 },
             { OsCode.VegaOs, 205 },
-            { OsCode.KoboOS, 205 },
+            { OsCode.KoboOS, 206 },
         };
 
         expectedValues.Count.ShouldBe(Enum.GetValues<OsCode>().Length);
