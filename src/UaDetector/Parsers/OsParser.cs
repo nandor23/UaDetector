6252ab09d3615d675f8e6d9c63371b42644b251e
Nandor Krizbai <krizbai.nandor@gmail.com>
2026-08-23 16:26:41 +0200

    Improve OS client hint mapping

--- src/UaDetector/Parsers/OsParser.cs
+++ src/UaDetector/Parsers/OsParser.cs
@@ -320,11 +320,12 @@ public sealed partial class OsParser : IOsParser
         ClientHintPlatformMappings = new Dictionary<string, FrozenSet<string>>
         {
             { OsNames.GnuLinux, new[] { "Linux" }.ToFrozenSet(StringComparer.OrdinalIgnoreCase) },
-            { OsNames.Mac, new[] { "MacOS" }.ToFrozenSet(StringComparer.OrdinalIgnoreCase) },
+            { OsNames.Mac, new[] { "MacOS", "Mac OS X", "MacIntel" }.ToFrozenSet(StringComparer.OrdinalIgnoreCase) },
             {
                 OsNames.PuffinOs,
                 new[] { "Cloud Phone 2.4" }.ToFrozenSet(StringComparer.OrdinalIgnoreCase)
             },
+            { OsNames.Windows, new[] { "Win32" }.ToFrozenSet(StringComparer.OrdinalIgnoreCase) },
         }.ToFrozenDictionary(StringComparer.OrdinalIgnoreCase);
 
         FireOsVersionMappings = new Dictionary<string, string>
